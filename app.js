2c3ddf8ae092c457446211d8d1647c00f22d9f7c
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-10-22 16:30:21 +0700

    revision routes, validate, and models users

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { passport } = require("./middlewares/auth");
-
-// const userRoutes = require("./routes/route/users");
-// const forgetPasswordRoutes = require("./routes/route/forgot_password");
-// const roleRoutes = require("./routes/route/roles");
-// const religionRoutes = require("./routes/route/religions");
-// const genderRoutes = require("./routes/route/genders");
-// const modulePermission = require("./routes/route/module_permission");
-// const moduleCategory = require("./routes/route/module_category");
-// const permissions = require("./routes/route/permissions");
-
 const routes = require("./routes/routes");
 
 //buat cors
@@ -35,14 +25,6 @@ app.use(cookieParser());
 app.use(passport.initialize());
 
 app.use("/", routes);
-
-// app.use("/", forgetPasswordRoutes);
-// app.use("/", roleRoutes);
-// app.use("/", religionRoutes);
-// app.use("/", genderRoutes);
-// app.use("/", modulePermission);
-// app.use("/", moduleCategory);
-// app.use("/", permissions);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

a81d2d84b17535d5ef4704a9a00fef9e406750ba
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-10-23 11:10:45 +0700

    implement validate by zod

--- routes/route/roles.js
+++ routes/route/roles.js
@@ -7,11 +7,16 @@ const {
   deleteRoles,
 } = require("../../controllers/roles");
 const router = express.Router();
+const {
+  validateMiddleware,
+  roleSchema,
+  updateRoleSchema,
+} = require("../../middlewares/validate");
 
-router.post("/role", createRoles);
+router.post("/role", validateMiddleware(roleSchema), createRoles);
 router.get("/role/:id", getRoleById);
 router.get("/role", getAllRoles);
-router.put("/role/:id", updateRoles);
+router.put("/role/:id", validateMiddleware(updateRoleSchema), updateRoles);
 router.delete("/role/:id", deleteRoles);
 
 module.exports = router;

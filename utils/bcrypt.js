953921dfc3481f26f645c043c7c9204cda0ea08f
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-10-29 09:26:00 +0700

    Initialize default permissions for role with empty permission data

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -3,12 +3,10 @@ const bcrypt = require("bcryptjs");
 salt = 10;
 async function hashPassword(password) {
   const hashedPassword = await bcrypt.hash(password, salt);
-  console.log(hashedPassword)
   return hashedPassword;
 };
 
 async function verifyPassword (password, hashedPassword) {
-  console.log(hashedPassword)
   if (!password || !hashedPassword) {
    throw new Error("Invalid password or hashedPassword");
   }

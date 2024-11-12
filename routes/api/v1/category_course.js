ae497ffc63a0cc1149b83a656a992a3e3e84c735
Nama Kamu <indahrizki016@gmail.com>
2024-11-13 04:03:39 +0700

    add validate for categories, sub-categories, course, module, sub-module course and materials

--- routes/api/v1/category_course.js
+++ routes/api/v1/category_course.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createCategory, deleteCategory } = require("../../../controllers/category_course");
+const {validateMiddleware, courseCategorySchema, deleteCourseCategorySchema } = require("../../../middlewares/validate");
 const router = express.Router();
 
-router.post("/categories", createCategory);
-router.delete("/categories/:id", deleteCategory);
+router.post("/categories", validateMiddleware(courseCategorySchema), createCategory);
+router.delete("/categories/:id", validateMiddleware(deleteCourseCategorySchema), deleteCategory);
 
 module.exports = router;
--- routes/api/v1/modules_course.js
+++ routes/api/v1/modules_course.js
@@ -6,11 +6,12 @@ const {
   getModuleById,
   getModuleByCourse,
 } = require("../../../controllers/modules_course");
+const {validateMiddleware, moduleCourseSchema, updateModuleCourseSchema, deleteModuleCourseSchema} = require("../../../middlewares/validate")
 const router = express.Router();
 
-router.post("/moduleCourse", createModuleCourse);
-router.put("/moduleCourse/:id", updateModuleCourse);
-router.delete("/moduleCourse/:id", deleteModuleCourse);
+router.post("/moduleCourse", validateMiddleware(moduleCourseSchema), createModuleCourse);
+router.put("/moduleCourse/:id", validateMiddleware(updateModuleCourseSchema), updateModuleCourse);
+router.delete("/moduleCourse/:id", validateMiddleware(deleteModuleCourseSchema), deleteModuleCourse);
 router.get("/moduleCourse/:id", getModuleById);
 router.get("/course/:id/module", getModuleByCourse);
 

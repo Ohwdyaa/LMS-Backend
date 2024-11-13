f16cf94f122352258ab8e8edeabe12f1a67d00c2
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-11-14 05:39:33 +0700

    feat: implement single endpoint for create and update operations

--- routes/api/v1/materials.js
+++ routes/api/v1/materials.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const { createMaterial, updateMaterial, deleteMaterial, getMaterialById } = require("../../../controllers/materials");
-const { validateMiddleware, materialsSchema, updateMaterialSchema, deleteMaterialSchema} = require("../../../middlewares/validate")
+const { updateMaterial, deleteMaterial, getMaterialById } = require("../../../controllers/materials");
+const { validateMiddleware, updateMaterialSchema, deleteMaterialSchema} = require("../../../middlewares/validate")
 const router = express.Router();
 
-router.post("/material", validateMiddleware(materialsSchema), createMaterial);
 router.put("/material/:id", validateMiddleware(updateMaterialSchema), updateMaterial);
 router.delete("/material/:id", validateMiddleware(deleteMaterialSchema), deleteMaterial);
 router.get("/material/:id", getMaterialById);

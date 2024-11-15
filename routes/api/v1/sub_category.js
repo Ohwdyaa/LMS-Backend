69660fc3b6a452539beca9b8f133a6c9c9a0918a
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-11-15 10:41:24 +0700

    feat: add feature update category and sub category course

--- routes/api/v1/sub_category.js
+++ routes/api/v1/sub_category.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { createSubCategory, deleteSubCategory } = require("../../../controllers/sub_category");
+const { createSubCategory, deleteSubCategory, updateSubCategory } = require("../../../controllers/sub_category");
 const { validateMiddleware, subCourseCategorySchema, deleteSubCourseCategorySchema }= require ("../../../middlewares/validate")
 const router = express.Router();
 
 router.post("/subCategory", validateMiddleware(subCourseCategorySchema), createSubCategory);
-router.delete("/categories/:id", validateMiddleware(deleteSubCourseCategorySchema), deleteSubCategory);
+router.put("/subCategory/:id", updateSubCategory);
+router.delete("/subCategory/:id", validateMiddleware(deleteSubCourseCategorySchema), deleteSubCategory);
 
 module.exports = router;

43dfdcb529ba2d1afff1f08addb52fa44d9a3ece
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-11-25 09:36:50 +0700

    chore: rename database connection configuration

--- config/config.js
+++ config/config.js
@@ -5,14 +5,14 @@ module.exports = {
   port: process.env.PORT,
   issuer: process.env.JWT_ISSUER,
   audience: process.env.JWT_AUDIENCE,
-  dbLmsManagement: {
+  dbLearningManagementSystem: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME1
   },
-  dbLmsModule: {
+  dbModulePages: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,

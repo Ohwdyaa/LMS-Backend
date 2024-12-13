4d096745a7390606b249698c8ccb53197a16c5b5
Widya Ayunindya Poge <107343788+Ohwdyaa@users.noreply.github.com>
2024-12-13 18:36:12 +0700

    change

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ const corsOptions = {
   credentials: true,
 };
 
-// const limiter = rateLimit({
-// 	windowMs: 15 * 60 * 1000, // 15 minutes
-// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-// 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-// });
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(helmet());
-// app.use(limiter);
+app.use(limiter);
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(routes);
